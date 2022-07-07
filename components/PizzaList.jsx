e202181c46a777c6c3f80b2d749bf30ea4edd597
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-07 16:06:37 -0700

    done

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from '../styles/PizzaList.module.css'
-import PizzaCard from "./PizzaCard"
+import PizzaCard from './PizzaCard'
 
 const PizzaList = ({pizzaList}) => {
   return (
