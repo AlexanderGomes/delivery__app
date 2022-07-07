3ed44ad514389f2b471d0c5e8a1457d757b107b6
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-07-07 16:13:49 -0700

    done

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -7,7 +7,7 @@ const PizzaList = ({pizzaList}) => {
     <div className={styles.container}>
     <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
     <p className={styles.desc}>
-    Do you think you have ever had a good pizza before ? you haven't met happiness yet, from monday to monday, 24/7, now you have the opportunity to have the best pizzas ever everyday !
+    Do you think you have ever had a good pizza before ? from monday to monday, 24/7, now you have the opportunity to have the best pizzas ever everyday !
     </p>
     <div className={styles.wrapper}>
       {pizzaList?.map((pizza) => (
